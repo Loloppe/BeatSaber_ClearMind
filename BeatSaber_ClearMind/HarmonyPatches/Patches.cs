e641d4503e80cde037fe1dd21f2cfe0a2ed5253b
Loloppe <loloppe2020@gmail.com>
2024-11-26 18:10:27 -0500

    Add project files.

---
+++ BeatSaber_ClearMind/HarmonyPatches/Patches.cs
@@ -0,0 +1,43 @@
+﻿
+using CameraUtils.Core;
+using HarmonyLib;
+using System;
+using System.Linq;
+
+namespace ClearMind.HarmonyPatches
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(StandardLevelDetailView), nameof(StandardLevelDetailView.RefreshContent))]
+        static class DetectExtensions
+        {
+            static void Postfix(StandardLevelDetailView __instance)
+            {
+                if(Config.Instance.Enabled == true && __instance.beatmapKey != null && __instance._beatmapLevel != null)
+                {
+                    var hasRequirement = SongCore.Collections.RetrieveDifficultyData(__instance._beatmapLevel, __instance.beatmapKey)?
+                    .additionalDifficultyData?
+                    ._requirements?.Any(x => x == "Noodle Extensions" || x == "Mapping Extensions") == true;
+                    if (hasRequirement) Config.Instance.Enabled = false;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(ObstacleController), nameof(ObstacleController.Init))]
+        static class HideObstacle
+        {
+            static void Prefix(ref ObstacleData obstacleData, ref StretchableObstacle ____stretchableObstacle)
+            {
+                if (Config.Instance.Enabled)
+                {
+                    if ((obstacleData.lineIndex > 3 && obstacleData.width >= 0) || (obstacleData.lineIndex < 0 && obstacleData.width + obstacleData.lineIndex <= 0))
+                    {
+                        if (Config.Instance.HideDesktopView) VisibilityUtils.SetLayerRecursively(____stretchableObstacle.gameObject, VisibilityLayer.Event);
+                        else VisibilityUtils.SetLayerRecursively(____stretchableObstacle.gameObject, VisibilityLayer.DesktopOnly);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ BeatSaber_ClearMind/Plugin.cs
@@ -0,0 +1,45 @@
+﻿using IPA;
+using Conf = IPA.Config.Config;
+using IPA.Config.Stores;
+using BeatSaberMarkupLanguage.GameplaySetup;
+using IPALogger = IPA.Logging.Logger;
+using HarmonyLib;
+using System.Reflection;
+
+namespace ClearMind
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance;
+        internal static IPALogger Log;
+        internal static Harmony harmony;
+
+        [Init]
+        public Plugin(Conf conf, IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            Config.Instance = conf.Generated<Config>();
+            harmony = new Harmony("Loloppe.BeatSaber.ClearMind");
+            BeatSaberMarkupLanguage.Util.MainMenuAwaiter.MainMenuInitializing += MainMenuInit;
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public void MainMenuInit()
+        {
+            GameplaySetup.Instance.AddTab("ClearMind", "ClearMind.UI.settings.bsml", Config.Instance, MenuType.All);
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+            harmony.UnpatchSelf();
+        }
+    }
+}
