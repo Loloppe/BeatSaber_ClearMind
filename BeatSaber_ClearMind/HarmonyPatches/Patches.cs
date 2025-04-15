df2134e4ffe8da78c0db12a60c2f0b21b0c55d8d
Loloppe <loloppe2020@gmail.com>
2025-04-15 19:48:45 -0400

    Multiplayer fix?

--- BeatSaber_ClearMind/HarmonyPatches/Patches.cs
+++ BeatSaber_ClearMind/HarmonyPatches/Patches.cs
@@ -16,29 +16,33 @@ namespace ClearMind.HarmonyPatches
                 if (Config.Instance.Enabled)
                 {
                     // This game seems to reuse object during gameplay, so need to set back parameters just in case.
-                    VisibilityUtils.SetLayerRecursively(__instance._stretchableObstacle.transform.Find("HideWrapper"), VisibilityLayer.Obstacle);
+                    var hw = __instance._stretchableObstacle.transform.Find("HideWrapper");
+                    VisibilityUtils.SetLayerRecursively(hw, VisibilityLayer.Obstacle);
                     var renderer = __instance._stretchableObstacle.transform.Find("ObstacleCore").GetComponent<MeshRenderer>();
-                    renderer.forceRenderingOff = false;
-
-                    // Option to make all walls transparent
-                    if (Config.Instance.ForceTransparent) renderer.forceRenderingOff = true;
-
-                    // Check to find non-gameplay wall.
-                    if ((obstacleData.lineIndex > 3 && obstacleData.width >= 0) || (obstacleData.lineIndex < 0 && obstacleData.width + obstacleData.lineIndex <= 0))
+                    if (renderer != null)
                     {
-                        if (Config.Instance.OutsideTransparent)
+                        renderer.forceRenderingOff = false;
+
+                        // Option to make all walls transparent
+                        if (Config.Instance.ForceTransparent) renderer.forceRenderingOff = true;
+
+                        // Check to find non-gameplay wall.
+                        if ((obstacleData.lineIndex > 3 && obstacleData.width >= 0) || (obstacleData.lineIndex < 0 && obstacleData.width + obstacleData.lineIndex <= 0))
                         {
-                            renderer.forceRenderingOff = true;
-                        }
-                        // This part hide the outline of the wall
-                        if (Config.Instance.HideDesktopView)
-                        {
-                            VisibilityUtils.SetLayerRecursively(__instance._stretchableObstacle.transform.Find("HideWrapper"), VisibilityLayer.Event);
-                        }
-                        else if (Config.Instance.HideOutline)
-                        {
-                            // The only issue with this logic is that the desktop view will see outer wall as transparent, while the rest are default.
-                            VisibilityUtils.SetLayerRecursively(__instance._stretchableObstacle.transform.Find("HideWrapper"), VisibilityLayer.DesktopOnly);
+                            if (Config.Instance.OutsideTransparent)
+                            {
+                                renderer.forceRenderingOff = true;
+                            }
+                            // This part hide the outline of the wall
+                            if (Config.Instance.HideDesktopView)
+                            {
+                                VisibilityUtils.SetLayerRecursively(hw, VisibilityLayer.Event);
+                            }
+                            else if (Config.Instance.HideOutline)
+                            {
+                                // The only issue with this logic is that the desktop view will see outer wall as transparent, while the rest are default.
+                                VisibilityUtils.SetLayerRecursively(hw, VisibilityLayer.DesktopOnly);
+                            }
                         }
                     }
                 }
